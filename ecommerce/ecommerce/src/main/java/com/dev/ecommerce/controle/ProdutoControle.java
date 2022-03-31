8bc130b53b32bb0b372decec7e168460c3899072
Stephani <73558275+stephani-ss@users.noreply.github.com>
2022-03-31 15:55:01 -0300

    Add files via upload
    API-94 #Remoção e alertas funcionando, adicionei lista.css pois o .show do boostrap estava apresentando conflitos com o .show usado na nav-bar.

--- ecommerce/ecommerce/src/main/java/com/dev/ecommerce/controle/ProdutoControle.java
+++ ecommerce/ecommerce/src/main/java/com/dev/ecommerce/controle/ProdutoControle.java
@@ -1,9 +1,12 @@
 package com.dev.ecommerce.controle;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,6 +32,13 @@ public class ProdutoControle {
 		ModelAndView mv = new ModelAndView("administrativo/produtos/lista");
 		mv.addObject("listaProdutos", produtoRepositorio.findAll());
 		return mv;
+	}
+	
+	@GetMapping("/administrativo/produtos/remover/{id}")
+	public ModelAndView remover(@PathVariable("id") Long id) {
+		Optional<Produto> produto = produtoRepositorio.findById(id);
+		produtoRepositorio.delete(produto.get());
+		return listar();
 	}
 	
 	@PostMapping("/administrativo/produtos/salvar")
