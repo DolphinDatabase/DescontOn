cf22d7aac9c437b8348d241b666065188972cb8f
Stephani <73558275+stephani-ss@users.noreply.github.com>
2022-04-06 09:02:03 -0300

    Add files via upload

---
+++ ecommerce/ecommerce/src/main/java/com/dev/ecommerce/controle/PrincipalControle.java
@@ -0,0 +1,18 @@
+package com.dev.ecommerce.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalControle {
+
+	@GetMapping("/administrativo")
+	public String acessarPrincipal() {
+		return "administrativo/home";
+	}
+	
+	@GetMapping("/administrador/home")
+	public String acessoTeste() {
+		return "administrativo/home";
+	}
+}
---
+++ ecommerce/ecommerce/target/classes/static/js/geral.js
@@ -0,0 +1,65 @@
+/*===== CADASTRO PRODUTO =====*/
+(function() {
+'use strict';
+ window.addEventListener('load', function() {
+// Pega todos os formulários para aplicar estilos de validação Bootstrap personalizados.
+ var forms = document.getElementsByClassName('needs-validation');
+// Faz um loop e evita o envio
+ var validation = Array.prototype.filter.call(forms, function(form) {
+ form.addEventListener('submit', function(event) {
+ if (form.checkValidity() === false) {
+ event.preventDefault();
+ event.stopPropagation();
+}
+ if (form.checkValidity() === true) {
+ alert("Produto Cadastrado com Sucesso");
+ }
+ form.classList.add('was-validated') ;
+  }, false);
+  });
+  }, false);
+  })();
+
+/*===== SACOLA DE COMPRAS =====*/ 				
+function finalizar(){
+alert("Compra Efetuada com Sucesso");
+}
+	
+document.addEventListener("DOMContentLoaded", function(event) {
+
+const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+const toggle = document.getElementById(toggleId),
+nav = document.getElementById(navId),
+bodypd = document.getElementById(bodyId),
+headerpd = document.getElementById(headerId)
+
+// Validate that all variables exist
+if(toggle && nav && bodypd && headerpd){
+toggle.addEventListener('click', ()=>{
+// show navbar
+nav.classList.toggle('show2')
+// change icon
+toggle.classList.toggle('bx-x')
+// add padding to body
+bodypd.classList.toggle('body-pd')
+// add padding to header
+headerpd.classList.toggle('body-pd')
+})
+}
+}
+
+showNavbar('header-toggle','nav-bar','body-pd','header')
+
+/*===== LINK ACTIVE =====*/
+const linkColor = document.querySelectorAll('.nav_link')
+
+function colorLink(){
+if(linkColor){
+linkColor.forEach(l=> l.classList.remove('active'))
+this.classList.add('active')
+}
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+//  to run since DOM is loaded and ready
+});
