fde7b832bde73639b10e93df51eab003ffbb4916
Stephani <73558275+stephani-ss@users.noreply.github.com>
2022-04-14 20:50:07 -0300

    Delete ecommerce/ecommerce directory

--- ecommerce/ecommerce/src/main/java/com/dev/ecommerce/repositorios/ItensCompraRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.dev.ecommerce.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.ecommerce.modelos.ItensCompra;
-
-public interface ItensCompraRepositorio extends JpaRepository<ItensCompra, Long> {
-}
--- ecommerce/ecommerce/src/main/java/com/dev/ecommerce/repositorios/ProdutoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dev.ecommerce.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.ecommerce.modelos.Produto;
-
-public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
-
-}
