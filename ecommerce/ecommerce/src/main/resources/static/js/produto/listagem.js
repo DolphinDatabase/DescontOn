624228e09252fa6c17152fc4000984c3726f9d3f
Stephani <73558275+stephani-ss@users.noreply.github.com>
2022-05-09 12:20:06 -0300

    Add files via upload
    API-122 #Correção lista

--- ecommerce/ecommerce/src/main/resources/static/js/produto/listagem.js
+++ ecommerce/ecommerce/src/main/resources/static/js/produto/listagem.js
@@ -10,12 +10,12 @@ async function popularTabela(){
         if(produto.status==0){
             var elemento = "<tr><td>"+produto.id+"</td><td>"+produto.nome+"</td><td>"+produto.categoria+"</td><td>"+produto.valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL',minimumFractionDigits: 2})+"</td>";
             elemento += "<td colspan='2'><a class='btn btn-info btn-sm' role='button' onclick='adicionarSacola("+JSON.stringify(produto)+")'><span class='bx bxs-shopping-bag' title='Adicionar na Sacola' aria-hidden='true'></span><td>";
-            elemento += "<a class='btn btn-sm promo' role='button'><span class='bx bxs-badge-dollar' title='Promoção' aria-hidden='true'></span></a>";
             elemento += "<a href='/administrativo/produto?id="+produto.id+"' class='btn btn-info btn-sm editar' role='button'><span class='bx bx-edit' title='Editar' aria-hidden='true'></span></a>";
+            elemento +="&nbsp;"
             elemento += "<a class='btn btn-sm btn-danger' data-toggle='modal' data-target='#modal-warning' onclick='abreModal("+produto.id+")'><span class='bx bxs-x-circle' title='Remover' aria-hidden='true'></span></a></td></tr>";
             $("#disp").append(elemento);
         }else{
-            var elemento="<tr><td>"+produto.id+"</td><td>"+produto.nome+"</td><td>"+produto.categoria+"</td><td>"+produto.valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL',minimumFractionDigits: 2})+"</td>"
+            var elemento="<tr><td>"+produto.id+"</td><td>"+produto.nome+"</td><td>"+produto.categoria+"</td><td>"+produto.valor.toLocaleString('pt-br',{style: 'currency', currency: 'BsRL',minimumFractionDigits: 2})+"</td>"
             elemento += "<td colspan='3'><a class='btn btn-info btn-sm editar' data-toggle='modal' data-target='#modal-warning'>";
             elemento += "<span class='bx bx-upload' title='desarquivar' onclick='desarquivar("+JSON.stringify(produto)+")' aria-hidden='true'></span></a></td></tr>";
             $("#arqui").append(elemento);
