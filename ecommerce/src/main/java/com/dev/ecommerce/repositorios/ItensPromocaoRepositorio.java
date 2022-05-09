c95a507b8e926c783ca0e2c602b0030bbe08f4bf
Beatriz Medeiros <74321890+beamedeiros@users.noreply.github.com>
2022-05-09 20:01:45 -0300

    Delete ecommerce/src directory
    API-197 #FOI CRIADO DUAS PASTAS ECOMMERCE E ESTAMOS ATUALIZANDO PARA CONTER APENAS A PASTA CERTA

--- ecommerce/src/main/java/com/dev/ecommerce/repositorios/ItensPromocaoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.dev.ecommerce.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.dev.ecommerce.modelos.ItensPromocao;
-
-public interface ItensPromocaoRepositorio extends JpaRepository<ItensPromocao, Long>{
-
-}
--- ecommerce/src/main/java/com/dev/ecommerce/repositorios/PromocaoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dev.ecommerce.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dev.ecommerce.modelos.Promocao;
-
-public interface PromocaoRepositorio extends JpaRepository<Promocao, Long> {
-
-}
